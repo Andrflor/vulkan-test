27450cdb2d39a5b354ecd19fcc48c7476c35e85d
andrflor <andrieu.florian@mail.com>
2023-02-11 17:10:41 +0100

    Added commands

--- vulkan.c
+++ vulkan.c
@@ -468,9 +468,91 @@ void vk_createFramebuffers(void) {
   }
 }
 
-void vk_createCommandPool(void) {}
+void vk_createCommandPool(void) {
+  uint32_t queueFamilyNumber = 0;
+  vkGetPhysicalDeviceQueueFamilyProperties(physicalDevice, &queueFamilyNumber,
+                                           VK_NULL_HANDLE);
 
-void vk_createCommandBuffer(void) {}
+  VkCommandPoolCreateInfo poolInfo = {};
+  poolInfo.sType = VK_STRUCTURE_TYPE_COMMAND_POOL_CREATE_INFO;
+  poolInfo.flags = VK_COMMAND_POOL_CREATE_RESET_COMMAND_BUFFER_BIT;
+  poolInfo.queueFamilyIndex = queueFamilyNumber;
+
+  VkResult result = vkCreateCommandPool(device, &poolInfo, NULL, &commandPool);
+  if (result != VK_SUCCESS) {
+    printf("Failed to create vk command pool\n");
+    exit(1);
+  }
+}
+
+void vk_createCommandBuffer(void) {
+  VkCommandBufferAllocateInfo allocInfo = {};
+  allocInfo.sType = VK_STRUCTURE_TYPE_COMMAND_BUFFER_ALLOCATE_INFO;
+  allocInfo.commandPool = commandPool;
+  allocInfo.level = VK_COMMAND_BUFFER_LEVEL_PRIMARY;
+  allocInfo.commandBufferCount = 1;
+
+  VkResult result =
+      vkAllocateCommandBuffers(device, &allocInfo, &commandBuffer);
+  if (result != VK_SUCCESS) {
+    printf("Failed to allocate vk command buffers\n");
+    exit(1);
+  }
+}
+
+void vk_recordCommandBuffer(VkCommandBuffer commandBuffer,
+                            uint32_t imageIndex) {
+  VkCommandBufferBeginInfo beginInfo = {};
+  beginInfo.sType = VK_STRUCTURE_TYPE_COMMAND_BUFFER_BEGIN_INFO;
+  beginInfo.flags = 0;               // Optional
+  beginInfo.pInheritanceInfo = NULL; // Optional
+
+  VkResult result = vkBeginCommandBuffer(commandBuffer, &beginInfo);
+  if (result != VK_SUCCESS) {
+    printf("Failed to begin recording vk command buffer\n");
+    exit(1);
+  }
+
+  VkRenderPassBeginInfo renderPassInfo = {};
+  renderPassInfo.sType = VK_STRUCTURE_TYPE_RENDER_PASS_BEGIN_INFO;
+  renderPassInfo.renderPass = renderPass;
+  renderPassInfo.framebuffer = framebuffers[imageIndex];
+  renderPassInfo.renderArea.offset = (VkOffset2D){0, 0};
+  renderPassInfo.renderArea.extent = swapChainExtent;
+
+  VkClearValue clearColor = {0.0f, 0.0f, 0.0f, 1.0f};
+  renderPassInfo.clearValueCount = 1;
+  renderPassInfo.pClearValues = &clearColor;
+
+  vkCmdBeginRenderPass(commandBuffer, &renderPassInfo,
+                       VK_SUBPASS_CONTENTS_INLINE);
+
+  vkCmdBindPipeline(commandBuffer, VK_PIPELINE_BIND_POINT_GRAPHICS, pipeline);
+
+  VkViewport viewport = {};
+  viewport.x = 0.0f;
+  viewport.y = 0.0f;
+  viewport.width = (float)swapChainExtent.width;
+  viewport.height = (float)swapChainExtent.height;
+  viewport.minDepth = 0.0f;
+  viewport.maxDepth = 1.0f;
+
+  VkRect2D scissor = {};
+  scissor.offset = (VkOffset2D){0, 0};
+  scissor.extent = swapChainExtent;
+
+  vkCmdSetViewport(commandBuffer, 0, 1, &viewport);
+  vkCmdSetScissor(commandBuffer, 0, 1, &scissor);
+
+  vkCmdDraw(commandBuffer, 3, 1, 0, 0);
+  vkCmdEndRenderPass(commandBuffer);
+
+  result = vkEndCommandBuffer(commandBuffer);
+  if (result != VK_SUCCESS) {
+    printf("Failed to record vk command buffer\n");
+    exit(1);
+  }
+}
 
 void vk_createSyncObjects(void) {}
 
@@ -490,6 +572,7 @@ void vk_init(void) {
 }
 
 void vk_cleanup(void) {
+  vkDestroyCommandPool(device, commandPool, NULL);
   size_t i;
   for (i = 0; i < swapChainImageCount; i++) {
     vkDestroyFramebuffer(device, framebuffers[i], NULL);
